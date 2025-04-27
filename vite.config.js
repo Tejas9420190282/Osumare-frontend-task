a80fff3fb1fcb39f431e455a1fa0b17f010c0f39
“Tejas9420190282” <tejasshimpi877@gmail.com>
2025-04-27 18:06:04 +0530

    update for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -6,3 +9,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
 })
+
+
